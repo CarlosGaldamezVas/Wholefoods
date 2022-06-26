b076c876def0ceadbc7dda8ad94f10135be71f47
DWWF <droidwarewolf8@gmail.com>
2022-06-26 04:31:59 -0400

    Create wholefoods.js

---
+++ wholefoods.js
@@ -0,0 +1,35 @@
+const product_container = document.querySelectorAll(".w-pie--product-tile"); //Product Container
+let data = [];
+
+product_container.forEach((el) => {
+  product_title = el.querySelector(
+    '[data-testid="product-tile-name"]'
+  ).textContent; // Product Title
+  product_price = el.querySelector(".regular_price").textContent;
+  product_brand = el.querySelector(
+    '[data-testid="product-tile-brand"]'
+  ).textContent;
+  wf_product_link = el
+    .querySelector('[data-testid="product-tile-link"]')
+    .getAttribute("href");
+  data.push({
+    Title: product_title,
+    Brand: product_brand,
+    Price: product_price,
+    Link: wf_product_link,
+  });
+});
+
+function downloadObjectAsJson(exportObj, exportName) {
+  var dataStr =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement("a");
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+downloadObjectAsJson(data, "Beef");
